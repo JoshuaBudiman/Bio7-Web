93a972e43f1917bbc9de1b7845c41a4a430ccb87
Joshua Kouwa Budiman <joshua.kbudiman@gmail.com>
2021-12-18 22:31:28 +0800

    Coba beberapa view

---
+++ resources/views/bab.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('main_content')
+<div class="container-sm">
+    <img class="img-fluid" src="img/logoBIO.png" alt="" width="128">
+</div>
+<br>
+<div class="container pt-5 my-5">
+
+    <button class="btn btn-warning m-5 p-3">
+        <h2>Bab ...</h2>
+        <p>Lorem Ipsum</p>
+    </button>
+
+</div>
+
+
+@endsection
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('main_content')
+<div class="text-center container-sm">
+    <img class="img-fluid mx-auto d-block" src="img/logoBIO.png" alt="" width="246">
+</div>
+<div class="text-center container-sm">
+    <button class="btn btn-warning align-center">
+        <img class="img-fluid mx-auto d-block" src="img/materiicon.png" alt="" width="150">
+    </button>
+</div>
+
+
+@endsection
