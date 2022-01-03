9567d7362c4daa11bb6acb5e175da62036fb97b6
strawhatcrue1 <84302030+strawhatcrue1@users.noreply.github.com>
2022-01-03 11:45:59 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [LoginController::class, 'login']);
+Route::post('refresh', [LoginController::class, 'refresh']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    //Route::apiResource('courses', CourseController::class);
+    //Route::apiResource('projects', ProjectController::class);
+    //Route::apiResource('users', ProfileController::class);
+    Route::post('logout', [LoginController::class, 'logout']);
 });
