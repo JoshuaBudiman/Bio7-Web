c8daee0f9fd588fdcfb71726cefaee339f6be4b4
michaellim1231 <michaellim1231@gmailcom>
2022-01-07 12:23:12 +0700

    insert soal di seeder

--- database/seeders/SoalSeeder.php
+++ database/seeders/SoalSeeder.php
@@ -54,5 +54,206 @@ class SoalSeeder extends Seeder
             'soal_text' => 'Makanan organisme klorofil berbeda dari makanan organisme non-klorofil karena organisme klorofil...',
             'stage_id' => '1'
         ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Perbedaan antara sel hewan dan tumbuhan dapat dilihat dari…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kesatuan struktural dan fungsional terkecil dari makhluk hidup disebut…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Peran pengurai dalam ekosistem adalah…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Gen yang merupakan faktor keturunan ditemukan di kromosom, sementara kromosom ditemukan di…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Sekumpulan semut di dalam lubang tanah membentuk…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Energi matahari sebelum sampai ke konsumen terlebih dahulu di simpan berupa makanan oleh…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Tanaman paku tanduk rusa yang menempel pada pohon – pohon sebagai tempat hidupnya disebut simbiosis…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Larutan iodium dapat membentuk hasil fotosintesis yang berbentuk.',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Peristiwa makan dan dimakan menurut urutan disebut…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kawasan yang berisi flora dan fauna langka disebut…?',
+            'stage_id' => '2'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Udara jika dilepaskan akan mengandung karbon dioksida dengan hasil dari pernafasan pada sel – sel dengan menggunakan…?',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => ' Perkembangan fungsi alat pada otak manusia yang berlangsung sangat cepat dan akan berlasung selama',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Makhluk hidup akan beradaptasi pada lingkungannya jika mampu melakukan.',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Perubahan karakteristik fisik pria dan wanita muda adalah…?',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Hormon androgen dan estrogen mulai aktif selama…?',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kerusakan fungsi organ – organ tubuh, yang dirasakan terutama oleh manusia pada waktunya…?',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Pernyataan mana yang salah…?',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Mampu mengiritasi berarti…?',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Karakteristik pada mahluk hidup akan memiliki sifatnya disebut sebagai.',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Makanan klorofil dari makanan non-klorofil jika terjadai organisme klorofil dalam bentuk.',
+            'stage_id' => '3'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Apa yang bukan karakteristik makhluk hidup adalah…?',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => ' Fotosintesis pada zat akan menghasilkan.',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Tanaman yang terbentuk dalam heterotropis disebut.',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Berikut ini adalah faktor-faktor yang membedakan tumbuhan dan hewan, dengan pengecualian…?',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Jaringan yang berbentuk meristematik pada hewan disebut.',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Pernyataan palsu berikut adalah…?',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'yang termasuk dalam fungsi golongan ikat disebut.',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Fotosintesis adalah contoh reaksi…?',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Hewan diklasifikasikan sebagai organisme heterotrofik karena…?',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kelompok hewan herbivora termasuk…?',
+            'stage_id' => '4'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Organel yang menghasilkan energi adalah…?',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Contoh organisme prokariotik adalah…?',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Berikut ini bentuk dari sel eukariotik adalah',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Organisme pada jaringan prokariotik yang membentuk organisme disebut.',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => ' Pernyataan palsu berikut adalah…?',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Sekelompok sel yang memiliki bentuk dan fungsi yang sama disebut…?',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Jaringan periderma berbentuk.',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Jaringan daun dapat melakukan gerakan fotosintesis disebut.',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Organel yang dapat melakukan membran dalam bentuk yang ganda adalah.',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Sel yang dapat menghasilkan sebagian besar mitokondria disebut
+            ',
+            'stage_id' => '5'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Makhluk hidup dan benda tak hidup dibedakan dengan adanya ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Berikut ini yang BUKAN merupakan ciri-ciri umum yang ditemukan pada makhluk hidup adalah …',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Makhluk hidup memperoleh energi dari ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kemampuan makhluk hidup untuk memperoleh keturunan disebut ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Berkembang biak bertujuan untuk  ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kemampuan makhluk hidup untuk menanggapi rangsangan disebut ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Diantara contoh iritabilitas adalah pada hewan tertentu memiliki alat indra khusus, seperti gurat sisi pada ikan yang berfungsi untuk ....',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Kemampuan makhluk hidup untuk menyesuaikan diri dengan lingkungan disebut ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Sesuatu yang mempunyai massa dan dapat menempati sebuah ruang ...',
+            'stage_id' => '6'
+        ]);
+        DB::table('soal')->insert([
+            'soal_text' => 'Berdasarkan wujudnya materi dapat dikelompokkan menjadi ...',
+            'stage_id' => '6'
+        ]);
     }
 }
