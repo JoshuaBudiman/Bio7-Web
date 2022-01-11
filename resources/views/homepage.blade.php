f1450947badbacee9999cf47996c4e71bfe6ae58
JhonThe17 <83646868+JhonThe17@users.noreply.github.com>
2022-01-11 20:10:51 +0900

    ....

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,11 +5,11 @@
     <img class="img-fluid mx-auto d-block" src="img/logoBIO.png" alt="" width="246">
 </div>
 <div class="text-center container-sm">
-    <a class="btn btn-danger align-center text-white fw-bold" href="{{route('material.index')}}">
+    <a class="btn align-center text-white fw-bold" href="{{route('material.index')}}">
         <img class="img-fluid mx-auto d-block rounded-corners" src="img/materiicon.png" alt="" width="150" >Materi
     </a>
 
-    <a class="btn btn-danger align-center text-white fw-bold" href="{{route('bab.index')}}">
+    <a class="btn align-center text-white fw-bold" href="{{route('bab.index')}}">
         <img class="img-fluid mx-auto d-block" src="img/quizicon.png" alt="" width="150">Quiz
     </a>
 </div>
